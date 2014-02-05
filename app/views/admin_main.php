6709239f16810293629bfc95ca1e056fe1947f98
Larry Eliemenye <denachural@gmail.com>
2014-02-05 06:41:34 +0000

    Some Major UI changes..Added Notification Dialog

--- app/views/admin_main.php
+++ app/views/admin_main.php
@@ -10,37 +10,42 @@
         <ul class="nav-menu">
             <li>
                 <a ui-sref="dashboard">
-                    Registered Users
+                    <span class="glyphicon glyphicon-th-large"></span>&nbsp;Dashboard
                 </a>
             </li>
             <li>
                 <a ui-sref="dashboard">
-
-                    Applicants
+                    <span class="glyphicon glyphicon-user"></span>&nbsp;Registered Users
                 </a>
             </li>
             <li>
                 <a ui-sref="dashboard">
-                    Under Review
+                    <span class="glyphicon glyphicon-check"></span>&nbsp;Applicants
                 </a>
             </li>
             <li>
                 <a ui-sref="dashboard">
-
-                    Approved
+                    <span class="glyphicon glyphicon-eye-open"></span>&nbsp;Under Review
+                </a>
+            </li>
+            <li>
+                <a ui-sref="dashboard">
+                    <span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;Approved
                 </a>
             </li>
             <li><a ui-sref="dashboard">
-                    Declined
+                    <span class="glyphicon glyphicon-thumbs-down"></span>&nbsp;Declined
                 </a>
-
-
             </li>
             <li>
                 <a ui-sref="dashboard">
-                    Settings
+                    <span class="glyphicon glyphicon-th-list"></span>&nbsp;Settings
                 </a>
-
+            </li>
+            <li style="border-top: 1px dashed #999;color: #005580">
+                <a ui-sref="dashboard">
+                    <span class="glyphicon glyphicon-log-out"></span>&nbsp;Log out
+                </a>
             </li>
         </ul>
     </div>
@@ -50,7 +55,23 @@
         </div>
     </div>
 </div>
-
+<div class="modal" id="sendNotificationModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content" style="background-color: #eee">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Send out Notifications</h4>
+            </div>
+            <div class="modal-body">
+                <textarea class="form-control" rows="7"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Send Now</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <script src="/js/ext/jquery.js"></script>
 <script src="/js/admin/js/bootstrap.js"></script>
@@ -60,7 +81,7 @@
 <script src="/js/admin.js"></script>
 <script>
     $('.view-control').tooltip({
-        placement:'left'
+        placement:'bottom'
     });
 </script>
 </body>
